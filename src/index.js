53bb0bdda90d77b373d5af1db918b4030cf94ecf
Volha Dazhenok <risadapetrovna@gmail.com>
2022-01-06 19:31:28 +0300

    Task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+  let res = '';
+  for (let i = str.length-1; i >= 0; i--) {
+    res += str[i];
+  }
+  return(parseInt(res))
 }
